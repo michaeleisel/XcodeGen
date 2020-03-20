94070701f8aadb58db1d811a5faa9af263fb1074
Michael Eisel <michael.eisel@gmail.com>
2020-03-20 12:06:35 -0400

    commit

--- Sources/XcodeGen/main.swift
+++ Sources/XcodeGen/main.swift
@@ -2,7 +2,17 @@ import Foundation
 import ProjectSpec
 import XcodeGenCLI
 import Version
+import QuartzCore
 
+let start = CACurrentMediaTime()
+
+print("go")
 let version = Version("2.14.0")
 let cli = XcodeGenCLI(version: version)
 cli.execute()
+let end = CACurrentMediaTime()
+print("time: \(end - start)")
+let a = "asdf"
+a.withCString { z in
+    write(1, z, 4)
+}
--- Sources/XcodeGenCLI/XcodeGenCLI.swift
+++ Sources/XcodeGenCLI/XcodeGenCLI.swift
@@ -2,6 +2,7 @@ import Foundation
 import ProjectSpec
 import SwiftCLI
 import Version
+import QuartzCore
 
 public class XcodeGenCLI {
     let cli: CLI
@@ -22,12 +23,15 @@ public class XcodeGenCLI {
     }
 
     public func execute(arguments: [String]? = nil) {
+        let start = CACurrentMediaTime()
         let status: Int32
         if let arguments = arguments {
             status = cli.go(with: arguments)
         } else {
             status = cli.go()
         }
+        let end = CACurrentMediaTime()
+        print("time: \(end - start)")
         exit(status)
     }
 }
